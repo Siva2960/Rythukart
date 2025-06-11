56b231b79bf641ad4abdef5de112b0831c3026a9
siva <siva193273@gmail.com>
2025-06-11 14:49:34 +0530

    Add Clever Cloud URL to ALLOWED_HOSTS

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -24,12 +24,9 @@ SECRET_KEY = 'django-insecure-586mu!2$(w923z^y&_^_^&6m7et&d_u%#f%)+jo5)=j!5l+(q#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+import os
 
-ALLOWED_HOSTS = [
-    'app_f6157d3c-5350-47b5-9efc-b1aa3bbc99ee.cleverapps.io',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 
